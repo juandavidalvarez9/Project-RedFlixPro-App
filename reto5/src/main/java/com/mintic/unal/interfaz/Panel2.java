8461b2b0c10cf20b0991b9dc8ba9a4615a768184
Fernan1122 <60754935+Fernan1122@users.noreply.github.com>
2020-12-01 15:57:09 -0500

    Add files via upload

---
+++ reto5/src/main/java/com/mintic/unal/interfaz/Panel2.java
@@ -0,0 +1,6 @@
+package com.mintic.unal.interfaz;
+
+import javax.swing.*;
+
+public class Panel2 extends JPanel {
+}
---
+++ reto5/src/main/java/com/mintic/unal/interfaz/Ventana.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mintic.unal.interfaz;
+import com.mintic.unal.reto5.Reto5Application;
+import java.util.Optional;
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana extends JFrame{
+
+    public Ventana() {
+        initComponents();
+        String [] args = {};
+        Reto5Application.runSpringServer(args);
+    }
+
+    /**
+     * Inicia la creación de las componentes de la ventana de la aplicación
+     */
+    private void initComponents() {
+        // Al cerrar la ventana se finaliza la ejecución del programa
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        // Establece el titulo de la ventana
+        setTitle("Base de Datos Redflix");
+
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            } catch (Exception ex) {
+
+            }
+        }
+
+        // Crea un panel que sirve de lienzo para el dibujo de la ciudad
+        Tabpanel panelPrincipal = new Tabpanel();
+        // Sirve de panel principal de la ventana
+        setContentPane(panelPrincipal);
+        // Adiciona el panel de controles al panel principal
+
+        // Adjusta el tamaño de la ventana para que quepan todos los componentes
+        pack();
+        setSize(700, 800);
+
+        // Permite ubicar la ventana en el centro de la pantalla
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension frameSize = getSize();
+        setLocation((screenSize.width - frameSize.width) / 2,
+                (screenSize.height - frameSize.height) / 2);
+
+        // Hace que la ventana de la aplicación sea visible
+        setVisible(true);
+    }
+}
